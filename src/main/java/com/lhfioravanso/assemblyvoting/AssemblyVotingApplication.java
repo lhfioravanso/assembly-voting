678ec64e4c596f8d550f6b9adb3ff80bdc1b1e55
lhfioravanso <leonardohf28@gmail.com>
2020-06-20 18:52:37 -0300

    initial commit

---
+++ src/main/java/com/lhfioravanso/assemblyvoting/AssemblyVotingApplication.java
@@ -0,0 +1,13 @@
+package com.lhfioravanso.assemblyvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblyVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssemblyVotingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lhfioravanso/assemblyvoting/AssemblyVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhfioravanso.assemblyvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblyVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
